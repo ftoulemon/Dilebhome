2d98bc0cf38a423a78ae2f19bbef6c0d25ef22ee
ftoulemon <florentin.toulemon@gmail.com>
2016-11-11 16:21:19 +0100

    Read data from mysql DB

---
+++ dbCon.php
@@ -0,0 +1,27 @@
+<?php
+    $username = "root";
+    $password = "";
+    $host = "chip";
+    $database="Electricity";
+
+    $server = mysql_connect($host, $username, $password);
+    $connection = mysql_select_db($database, $server);
+
+    $myquery = "SELECT  `ts`, `value` FROM `records`";
+    $query = mysql_query($myquery);
+
+    if ( ! $query ) {
+        echo mysql_error();
+        die;
+    }
+
+    $data = array();
+
+    for ($x = 0; $x < mysql_num_rows($query); $x++) {
+        $data[] = mysql_fetch_assoc($query);
+    }
+
+    echo json_encode($data);
+
+    mysql_close($server);
+?>
