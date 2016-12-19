e1efcd83be6ca3dbd7330256a8cd9ed44b696050
ftoulemon <florentin.toulemon@gmail.com>
2016-12-19 21:14:29 +0000

    Setup crontab
    Signed-off-by: ftoulemon <florentin.toulemon@gmail.com>

--- backend/start.py
+++ backend/start.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 # coding: utf-8
 
+import os
 import sys
 import signal
 import logging
 import argparse
+
+from crontab import CronTab
 
 from lib.DbConnector import DbConnector
 from lib.Acquisition import Acquisition
@@ -27,10 +30,36 @@ def signalHandler(signal, frame):
     acq.Stop()
 
 
+def SetupCrontab():
+    """
+    Setup the crontab.
+    """
+    logging.info("Setup crontab")
+    wFile = os.path.abspath(__file__)
+    wCron = CronTab(tabfile='/etc/crontab', user=False)
+    # Clear existing jobs, if any
+    wCron.remove_all(command=wFile)
+    # Create jobs
+    wJobminute = wCron.new(command=wFile + ' -p minute', user='root')
+    wJobminute.setall('* * * * *')
+    wJobhour = wCron.new(command=wFile + ' -p hour', user='root')
+    wJobhour.setall('@hourly')
+    wJobday = wCron.new(command=wFile + ' -p day', user='root')
+    wJobday.setall('@daily')
+    wJobmonth = wCron.new(command=wFile + ' -p month', user='root')
+    wJobmonth.setall('@monthly')
+    wJobyear = wCron.new(command=wFile + ' -p year', user='root')
+    wJobyear.setall('@yearly')
+    # Save
+    wCron.write()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
             description = 'Start acquisition'
             )
+    parser.add_argument("-s", "--setup", help="Setup crontab and exit",
+            action="store_true", default=False)
     parser.add_argument("-v", "--verbose", help="Verbositiy setting",
             action="count", default=0)
     parser.add_argument("-p", "--period", help="Period to record",
@@ -51,15 +80,18 @@ if __name__ == "__main__":
     logging.info('Started')
     # signal handling
     signal.signal(signal.SIGINT, signalHandler)
-    # start things
-    with DbConnector() as wDb:
-        wDb.Init()
-    # data
-    wFrame = acq.GetData()
-    acq.ProcessFrame(dicTable[args.period], wFrame)
-    # system monitoring
-    if args.period == 'minute':
-        sm.Monitor()
+    if args.setup is True:
+        SetupCrontab()
+        # start things
+        with DbConnector() as wDb:
+            wDb.Init()
+    else:
+        # data
+        wFrame = acq.GetData()
+        acq.ProcessFrame(dicTable[args.period], wFrame)
+        # system monitoring
+        if args.period == 'minute':
+            sm.Monitor()
     # exit things
     sys.exit(0)
 
